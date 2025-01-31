b807e29b6f48843c859686af1b3fde17d418efc7
Keshavsharma50009 <keshav1712.becse24@chitkara.edu.in>
2025-01-31 20:31:44 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -7,7 +7,7 @@ int main(){
         printf("A");
     }
     else if(80>=a<90){printf("B");}
-    else if(70>=a<80){printf("c");}
+    else if(70>=a<80){printf("C");}
     else if(60>=a<70){printf("D");}
     else if(a<60){printf("F");}
     return 0;
