312476e4c815b224d2bf470559d85c1e52ae2b44
Keshavsharma50009 <keshav1712.becse24@chitkara.edu.in>
2025-02-01 23:02:36 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,9 +4,9 @@ int main(){
     char c;
     scanf("%f %f %c",&a,&b,&c);
     if( c == '+'){
-        printf(" %.f",a + b);         
+        printf(" %.0f",a + b);         
     }else if( c == '-'){
-        printf(" %.0f", a - b);
+        printf(" %f", a - b);
     }else if( c == '*'){
         printf(" %.0f",a * b);
     }else if( c == '/'){
