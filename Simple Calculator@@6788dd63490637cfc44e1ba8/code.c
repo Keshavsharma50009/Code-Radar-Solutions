8e7d7548fd3ec4b79856fda47dd7cec423152fb7
Keshavsharma50009 <keshav1712.becse24@chitkara.edu.in>
2025-02-01 23:02:07 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,44 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-float a,b;
+    float a,b;
     char c;
-    scanf("%f %f %c",&a, &b, &c);
-    // if(c == '+'){
-        // printf("%.0f", a + b);
-
-    // }else if( c == '-'){
-        // printf("%.0f",a - b);
-    // }else if(c == '*'){
-        // printf("%.0f",a * b);
-    // }else if( c == '/'){
-        // printf("%.0f",a / b);
-    // }else{
-
-    // }
-
-    //if(c=='+'){printf("%.0f\n",a+b);}
-    //else if (c=='-'){printf("%.0f\n",a-b);}
-   // else if(c=='*'){printf("%.0f\n",a*b);}
-   // else if (c=='/'){if((b!=0)){printf("%.0f\n",a/b);}
-    //else{printf("Invalid\n");}}
-    //else{printf("Invalid");}
-    //return 0;
-switch(c){
-     case '+':
-     printf("%.0f\n",a+b);
-     break;
-     case '-':
-     printf("%.0f\n",a-b);
-     break;
-     case '*':
-     printf("%.0f\n",a*b);
-     break;
-     case '/':
-      printf("%.0f\n",a/b);
-     break;
-     default:
-     printf("Error");
-}
-
+    scanf("%f %f %c",&a,&b,&c);
+    if( c == '+'){
+        printf(" %.0f",a + b);         
+    }else if( c == '-'){
+        printf(" %.0f", a - b);
+    }else if( c == '*'){
+        printf(" %.0f",a * b);
+    }else if( c == '/'){
+        printf(" %.0f", a / b);
+    }else{
+        printf("error")
+    }
     return 0;
 }
