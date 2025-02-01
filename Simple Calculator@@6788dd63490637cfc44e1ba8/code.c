48d46032a2b3ff09a9761ffb9f2c913119097728
Keshavsharma50009 <keshav1712.becse24@chitkara.edu.in>
2025-02-01 18:58:04 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,11 +3,28 @@ int main(){
     float a,b;
     char c;
     scanf("%f %f %c",&a, &b, &c);
-    if(c=='+'){printf("%.0f\n",a+b);}
-    else if (c=='-'){printf("%.0f\n",a-b);}
-    else if(c=='*'){printf("%.0f\n",a*b);}
-    else if (c=='/'){if((b!=0)){printf("%.0f\n",a/b);}
-    else{printf("Invalid\n");}}
-    else{printf("Invalid");}
-    return 0;
+    //if(c=='+'){printf("%.0f\n",a+b);}
+    //else if (c=='-'){printf("%.0f\n",a-b);}
+   // else if(c=='*'){printf("%.0f\n",a*b);}
+   // else if (c=='/'){if((b!=0)){printf("%.0f\n",a/b);}
+    //else{printf("Invalid\n");}}
+    //else{printf("Invalid");}
+    //return 0;
+switch(c){
+    case '+':
+    printf("%.0f\n",a+b);
+    break;
+    case '-':
+    printf("%.0f\n",a-b);
+    break;
+    case '*':
+    printf("%.0f\n",a*b);
+    break;
+    case '/':
+    printf("%.0f\n",a/b);
+    break;
+    default:
+    printf("Invalid");
+}
+return 0;
 }
