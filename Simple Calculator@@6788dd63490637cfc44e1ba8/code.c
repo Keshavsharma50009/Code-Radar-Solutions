6200e1df7e5df082e0ac5aa68174d782c2ec7139
Keshavsharma50009 <keshav1712.becse24@chitkara.edu.in>
2025-02-01 23:03:34 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -8,7 +8,7 @@ int main(){
     }else if( c == '-'){
         printf(" %.0f", a - b);
     }else if( c == '*'){
-        printf(" %.2f",a * b);
+        printf(" %.0f",a * b);
     }else if( c == '/'){
         printf(" %.0f", a / b);
     }else{
