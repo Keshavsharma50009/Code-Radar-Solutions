5105f3d440f29395aab4298a65f3a6993537b39d
Keshavsharma50009 <keshav1712.becse24@chitkara.edu.in>
2025-02-05 17:07:14 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,24 +1,61 @@
+// #include <stdio.h>
+// int main(){
+// int n;
+// scanf("%d",&n);
+// for(int i=1;i<n+1;i++){
+//     for (int j=1;j<=n-i;j++){
+//         printf(" ");
+//     }
+//     for(int j=1;j<=(2*i)-1;j++){
+//         printf("*");
+//     }
+//     printf("\n");
+// }
+// for(i=n-1;i>=1;i--){
+//     for(j=1;j<n-i;j++){
+//         printf(" ");
+//     }
+//     for(j=1;j<=2*i-1;j++){
+//         printf("*");
+//     }
+//     printf("\n");
+// }
+// return 0;
+// }
+
+
+
 #include <stdio.h>
-int main(){
-int n;
-scanf("%d",&n);
-for(int i=1;i<n+1;i++){
-    for (int j=1;j<=n-i;j++){
-        printf(" ");
+
+int main() {
+    int n;
+    scanf("%d", &n);  // Read the input for the number of rows for the top half of the diamond
+
+    // Upper part of the diamond (including the middle row)
+    for (int i = 1; i <= n; i++) {  // Start from i = 1 instead of i = 0
+        // Print spaces before the stars in each row
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // Print stars in the current row
+        for (int j = 1; j <= 2 * i - 1; j++) {  // The number of stars increases as i increases
+            printf("*");
+        }
+        printf("\n");  // Move to the next row
     }
-    for(int j=1;j<=(2*i)-1;j++){
-        printf("*");
+
+    // Lower part of the diamond
+    for (int i = n - 1; i >= 1; i--) {
+        // Print spaces before the stars in each row
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // Print stars in the current row
+        for (int j = 1; j <= 2 * i - 1; j++) {  // The number of stars decreases as i decreases
+            printf("*");
+        }
+        printf("\n");  // Move to the next row
     }
-    printf("\n");
-}
-for(i=n-1;i>=1;i--){
-    for(j=1;j<n-i;j++){
-        printf(" ");
-    }
-    for(j=1;j<=2*i-1;j++){
-        printf("*");
-    }
-    printf("\n");
-}
-return 0;
+
+    return 0;
 }
