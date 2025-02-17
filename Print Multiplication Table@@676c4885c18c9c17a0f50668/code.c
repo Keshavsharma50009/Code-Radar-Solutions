47d15fbf14de35dadaf5f7c76832b803f4e0e823
Keshavsharma50009 <keshav1712.becse24@chitkara.edu.in>
2025-02-17 22:51:03 +0530

    Solution to Print Multiplication Table

---
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    pro=1;
+    for(int i=1;i<=10;i++){
+        pro=pro*i;
+        printf("%d",a x i = pro);
+        printf("\n");
+    }
+    return 0;
+}
