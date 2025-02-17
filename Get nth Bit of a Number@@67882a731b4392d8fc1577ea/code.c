98371c03b09578a9215a34c92fb6c19f21b47e6c
Keshavsharma50009 <keshav1712.becse24@chitkara.edu.in>
2025-02-17 20:11:45 +0530

    Solution to Get nth Bit of a Number

--- Get nth Bit of a Number@@67882a731b4392d8fc1577ea/code.c
+++ Get nth Bit of a Number@@67882a731b4392d8fc1577ea/code.c
@@ -3,8 +3,8 @@ int main(){
     int a,b;
     scanf("%d%d",&a,&b);
     if((a<<b)&1){
-        printf("Set");
+        printf("1");
     }
-    else{printf("Not Set");}
+    else{printf("0");}
     return 0;
 }
