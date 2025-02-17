469d84d1adb879536c9e927d4a9e2cdbe0b461df
Keshavsharma50009 <keshav1712.becse24@chitkara.edu.in>
2025-02-17 19:51:29 +0530

    Solution to Check Opposite Signs

--- Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -2,7 +2,7 @@
 int main(){
     int a,b;
     Scanf("%d %d",&a,&b);
-    if((a>0&&b>00)||(a<0&&b<0)){printd("False");}
+    if((a>0 && b>00)||(a<0 && b<0)){printf("False");}
     else{printf("True");}
     return 0;
 }
